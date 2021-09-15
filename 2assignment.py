6a620bab7e570e9e0e16c578256f62a91e366b2d
Ese Okogu <ejirookogu@gmail.com>
2021-09-15 10:45:52 +0100

    assignment2.py

--- 2assignment.py
+++ 2assignment.py
@@ -1,3 +1,19 @@
 s1 = "Abc"
 s2 = "Xyz"
-print(s1[0]+s2[2]+s1[1]+s2[1]+s1[2]+s2[0])
+#print(s1[0]+s2[2]+s1[1]+s2[1]+s1[2]+s2[0])
+
+
+#question 3
+s1 = "Abc"
+s2 = "Xyz"
+s2 = s2[::-1]
+lengthS1 = len(s1)
+lengthS2 = len(s2)
+length = lengthS1 if lengthS1 > lengthS2 else lengthS2
+result_string =" "
+for i in range(length):
+  if(i < lengthS1):
+      result_string = result_string + s1[i]
+  if(i < lengthS2):
+      result_string = result_string + s2[i]
+print("The new string is \n\t\t", result_string)
