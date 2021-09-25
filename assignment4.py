161383dc235cab307bd4d15c62bfcc74003b587d
Ese Okogu <ejirookogu@gmail.com>
2021-09-25 11:12:05 +0100

    set operation

--- assignment4.py
+++ assignment4.py
@@ -1,6 +1,6 @@
 E = int(input("Enter the total number of students that subscibed to an English newspaper:\n"))
 English = set(input("enter roll numbers:\n").split())
-F = int(input("Enter the total number of students that subscibed to an French newspaper:\n"))
+F = int(input("Enter the total number of students that subscibed to a French newspaper:\n"))
 French = set(input("Enter roll number:\n").split())
 final = English.intersection(French)
 print(len(final))
